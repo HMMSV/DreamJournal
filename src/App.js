b63d7c62cad553bd581ba210e8a51aea304a1d59
HV <heather.m.voisin@gmail.com>
2022-12-01 13:50:37 -0500

    initial dev branch commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import { useEffect } from 'react';
-import axios from 'axios';
 import './App.css';
 
 function App() {
 
 useEffect(() => {
     
-  axios({
-      url: "https://type.fit/api/quotes",
-      method: "GET",
-      dataResponse: "json",
-      params: {
-        format: "json",
-      },
-    }).then((response) => {
-      console.log(response);
-    });
+
   });
 
 
